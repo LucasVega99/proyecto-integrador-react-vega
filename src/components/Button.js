edf9ad260f54f8b15880821fcc1f8ca0fc0fbc5e
Lucas Vega <lucas.vega2399@gmail.com>
2021-11-19 12:04:47 -0300

    NavBar 2

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import './Button.css'
+const Button = ({label}) => {
+    return (
+        <button className='button'>{label}</button>
+        )
+}
+
+export default Button
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,25 +1,19 @@
 import './NavBar.css';
+import Button from './Button';
 
 const NavBar = () => {
     return (
         <nav className="nav-bar">
             <div className="logo">
-                <h2>Flexbox Footwear</h2>
+            <a><h2>Flexbox Footwear</h2></a>
             </div>
             ­<ul className="lista-nav">
                 <a><li>Hombre</li></a>
                 <a><li>Mujer</li></a>
                 <a><li>Niños</li></a>
                 <a><li>Deportes</li></a>
-                <div className="drop-down-menu">
-                    <div className="drop-down-menu-content">
-                        <a href="">Running</a>
-                        <a href="">Urbano</a>
-                        <a href="">Futbol</a>
-                        <a href="">Training</a>
-                    </div>
-                </div>
             </ul>
+            <Button label='Ingresar' />
         </nav>
     )
 }
