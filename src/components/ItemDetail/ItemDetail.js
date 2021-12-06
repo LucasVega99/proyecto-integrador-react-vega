5fc64e9c4694d52dd984b01ca16012a5685541a8
Lucas Vega <lucas.vega2399@gmail.com>
2021-12-06 17:02:31 -0300

    Item

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -7,6 +7,7 @@ const ItemDetail = ({item}) => {
                 <div className='detalle-item'>
                     <div><h3 key={o.id}>{o.nombre}</h3></div>
                     <div className='contenedor-imagen-item'><img className='imagen-item' src={o.imagen} /> </div>
+                    <div><p>Las Zapatillas Nike Air Zoom Gravity con una capellada completamente transpirable tienen un diseño elegante y estilizado para que luzcas a la moda mientras tus pisadas amortiguadas te dan velocidad y estabilidad mientras corres, entrenas o estas en tus actividades diarias.</p></div>
                     <div><p>{o.precio}</p></div>
                     <Button label='Comprar' />
                 </div>)}
